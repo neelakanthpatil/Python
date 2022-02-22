84f6607f2b7454c5dad2a0b64bc11b5f83ddde5e
neelakanthpatil <patil.neelakanth@gmail.com>
2022-02-22 21:51:05 +0530

    Create hello.py
    hello.py first version commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+## This is my first python program
+## As per software industry rituals i am also starting with "Hello World" program.
+
+import sys
+
+def main():
+  print ('Hello, World')
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
