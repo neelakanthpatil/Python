82ff9d8a18f1b5bca22fd12f5073d6a326fa1c19
neelakanthpatil <patil.neelakanth@gmail.com>
2022-02-23 20:37:52 +0530

    Update hello.py
    if else usage

--- hello.py
+++ hello.py
@@ -4,8 +4,16 @@
 import sys
 
 def main():
-  fname = sys.argv[1]
-  lname = sys.argv[2]
+  if len(sys.argv)<2:
+    fname = 'No Parameters'
+  else:
+    fname = sys.argv[1]
+    
+  if len(sys.argv)<2:
+    lname = 'Passed'
+  else:
+    lname = sys.argv[2]
+    
   print ("The script Name is", sys.argv[0])
   print ('Hello,', fname, lname)
 
