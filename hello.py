04f76f2abc7463790fa856eb14de694f2500343c
neelakanthpatil <patil.neelakanth@gmail.com>
2023-01-14 08:06:55 +0530

    New Changes

--- hello.py
+++ hello.py
@@ -4,18 +4,8 @@
 import sys
 
 def main():
-  if len(sys.argv)<2:
-    fname = 'No Parameters'
-  else:
-    fname = sys.argv[1]
-    
-  if len(sys.argv)<2:
-    lname = 'Passed'
-  else:
-    lname = sys.argv[2]
-    
   print ("The script Name is", sys.argv[0])
-  print ('Hello,', fname, lname)
+  print ('Hello,World...!!!')
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
